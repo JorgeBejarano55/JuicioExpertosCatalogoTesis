24c29bb65884f891f03fa63ec77250f7f6f3e8d6
Jorge Bejarano <jorgeguar@unicauca.edu.co>
2024-07-04 09:44:18 -0500

    Inicio de proyecto

---
+++ formulario-olores-comunitarios/src/app/form-reactive/form-reactive.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-reactive',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-reactive.component.html',
+  styleUrl: './form-reactive.component.css'
+})
+export class FormReactiveComponent {
+
+}
